67b52a92eb959011efc3d5261c004fee3c197d60
samuelw0624 <samuelw0624@hotmail.com>
2022-10-13 13:54:00 -0400

    Create game manager
    manages card spawning functionality

--- Programming_Pattern_Card_Game/Assets/Scripts/GameManager.cs
+++ Programming_Pattern_Card_Game/Assets/Scripts/GameManager.cs
@@ -1,9 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> deck = new List<GameObject>();
+    public Transform[] cardSlots;
+    public bool[] availableSlots;
+
+    public TextMeshProUGUI deckSizeCount;
+
+    public void DrawCard()
+    {
+        if(deck.Count >= 1)
+        {
+            GameObject randomCard = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableSlots.Length; i++)
+            {
+                if(availableSlots[i] == true)
+                {
+                    Instantiate(randomCard, cardSlots[i].position, Quaternion.identity);
+                    availableSlots[i] = false;
+                    deck.Remove(randomCard);
+                    return;
+                }
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +39,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        deckSizeCount.text = deck.Count.ToString();
     }
 }
